efe9c4a79b75cfb2478a78c7a933c10305b426bf
b1017120 <b1017120@STB1017120>
2017-05-07 04:20:12 +0900

    プロジェクト ファイルを追加します。

---
+++ ShiranuiTwitterBot/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CoreTweet;
+
+namespace ShiranuiTwitterBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var tokens = Tokens.Create();
+            tokens.Statuses.UpdateAsync(status => "ShiranuiTest").Wait();
+        }
+    }
+}
